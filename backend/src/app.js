2768e85e54ee849bca4630d0f293932a90b5ecb5
Jorge William <william75762@gmail.com>
2024-04-23 22:47:42 -0300

    Primeiro commit

---
+++ backend/src/app.js
@@ -0,0 +1,19 @@
+/*const express = require('express');
+const router = require('./router')
+const app= express();
+
+app.use(router);
+
+module.exports = app;*/
+
+const express = require('express');
+const cors = require('cors');
+const router = require('./router');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(router);
+
+module.exports = app;
